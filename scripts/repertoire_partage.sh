fbfb41f01e08663aef25e2c743fc5998e7392ff3
ariabtsev <alexandre.riabtsev@gmail.com>
2022-12-19 17:16:55 +0100

    Utilisation de l'ISO incluse dans VB pour additions

--- scripts/repertoire_partage.sh
+++ scripts/repertoire_partage.sh
@@ -6,7 +6,7 @@ ISO_MOUNT_DIR="/media/vbox_guest_additions"
 echo "PiecesNumeriques $CTRL_DESKTOP/PartagePiecesNumeriques vboxsf defaults,_netdev 0 0" >>"/etc/fstab"
 
 # Installation des paquets
-apt install -y virtualbox-guest-additions-iso
+# apt install -y virtualbox-guest-additions-iso
 
 # Création des répertoires
 mkdir -p "$ISO_MOUNT_DIR"
@@ -14,7 +14,8 @@ mkdir -p "$CTRL_DESKTOP/PartagePiecesNumeriques"
 chown -R "$CTRL_USERNAME:$CTRL_GROUP" "$CTRL_DESKTOP/PartagePiecesNumeriques"
 
 # Montage du CD des extensions VirtualBox
-mount -o ro "/usr/share/virtualbox/VBoxGuestAdditions.iso" "$ISO_MOUNT_DIR"
+#mount -o ro "/usr/share/virtualbox/VBoxGuestAdditions.iso" "$ISO_MOUNT_DIR"
+mount "/dev/sr0" "$ISO_MOUNT_DIR"
 
 # Installation des extensions VirtualBox
 "$ISO_MOUNT_DIR/autorun.sh"
